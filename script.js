d1961954f5e910c5366433845c13caa8eabc99ab
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-01-31 23:29:03 -0300

    add btn apagar

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 let agenda = [];
 
-function conferirFormulario() {
-  const inputNome = document.getElementById("inputNome").value;
-  const inputData = document.getElementById("inputData").value;
-  const msgErroNome = document.getElementById("msgErroNome");
-  const msgErroData = document.getElementById("msgErroData");
-
-  if (inputNome.length < 3 || inputNome.length > 120) {
-    msgErroNome.style.display = "flex";
-  }
-  if (inputData.length < 10) {
-    msgErroData.style.display = "flex";
-  }
-
-  guardarDados();
-  enviarFormulario();
-}
-
 function onlyNumber(event) {
   const regex = /\D/g;
 
@@ -34,24 +17,45 @@ function maskData(inputData) {
   }
 }
 
+function conferirFormulario() {
+  const inputNome = document.getElementById("inputNome").value;
+  const inputData = document.getElementById("inputData").value;
+  const msgErroNome = document.getElementById("msgErroNome");
+  const msgErroData = document.getElementById("msgErroData");
+
+  if (inputNome.length < 3 || inputNome.length > 120) {
+    msgErroNome.style.display = "flex";
+  }
+  if (inputData.length < 10) {
+    msgErroData.style.display = "flex";
+  }
+
+  guardarDados();
+  enviarFormulario();
+}
+
+let id = 1;
+
 function enviarFormulario() {
   const inputNome = document.getElementById("inputNome").value;
   const inputData = document.getElementById("inputData").value;
   const contBody = document.getElementById("contBody");
-  let id = 1;
+  let novoId = `row_${id}`;
 
   contBody.innerHTML += `
-  <tr>
-    <td id="nome">${inputNome}</td>
-    <td>${inputData}</td>
-    <td>
-      <button class="btnEditar">Editar</button>
-      <button class="btnDeletar">Deletar</button>
-    </td>
-  </tr>
+    <tr id="${novoId}">
+      <td id="nome">${inputNome}</td>
+      <td>${inputData}</td>
+      <td>
+        <button class="btnEditar">Editar</button>
+        <button class="btnDeletar">Deletar</button>
+      </td>
+    </tr>
   `;
 
   id++;
+
+  apagarAniversario();
 }
 
 function guardarDados() {
@@ -62,14 +66,25 @@ function guardarDados() {
   btnSalvar.addEventListener("click", () => {
     agenda.push({ nome, data });
   });
-  localStorage.setItem("pessoas", JSON.stringify(agenda));
-  agenda = JSON.parse(localStorage.getItem("pessoas"));
+
+  localStorage.setItem("aniversarios", JSON.stringify(agenda));
+  agenda = JSON.parse(localStorage.getItem("aniversarios"));
 }
 
-// function editarAniversario() {
-//   const editarButton = document.getElementsByClassName("btnEditar");
-//   editarButton.addEventListener("click", () => {
-//     console.log(editarButton);
-//   });
-// }
-// editarAniversario();
+function apagarAniversario() {
+  const editarButton = document.querySelectorAll(".btnDeletar");
+
+  editarButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      const row = event.target.closest("tr");
+      const nome = row.querySelector("#nome").textContent;
+      const data = row.querySelectorAll("td")[1].textContent;
+
+      document.getElementById("inputNome").value = nome;
+      document.getElementById("inputData").value = data;
+
+      row.remove();
+    });
+  });
+}
+apagarAniversario();
